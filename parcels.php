d5e86a82273bc908009e63d493f7b6e4d0dae05f
Aundra Miller and Mollie Bootsma <miller.aundra@gmail.com>
2016-02-08 12:05:15 -0800

    add PHP validation

--- parcels.php
+++ parcels.php
@@ -72,6 +72,7 @@
 <!DOCTYPE html>
 <html>
   <head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
     <meta charset="utf-8">
     <title></title>
   </head>
@@ -88,19 +89,25 @@
             $parcel_info = array($parcel_height, $parcel_width, $parcel_length, $parcel_weight);
 
             foreach($parcel_info as $input) {
+                $completed = 0;
                 if ($input == 0) {
-                    echo "<h2>Please fill in all fields.</h2>";
+                    $completed = $completed + 1;
+                } else {
+                    $completed = $completed;
                 }
             }
 
-            echo "<li> Height: $parcel_height inches </li>
-                  <li> Width: $parcel_width inches </li>
-                  <li> Length: $parcel_length inches </li>
-                  <li> Volume: $parcel_volume cubic inches</li>
-                  <li> Weight: $parcel_weight pounds</li>
-                  <h2>Total cost to ship: $$parcel_cost </h2>
-                  ";
-
+            if ($completed != 0) {
+                echo "<h2>Please fill in all form fields.</h2>";
+            } else {
+                echo "<li> Height: $parcel_height inches </li>
+                      <li> Width: $parcel_width inches </li>
+                      <li> Length: $parcel_length inches </li>
+                      <li> Volume: $parcel_volume cubic inches</li>
+                      <li> Weight: $parcel_weight pounds</li>
+                      <h2>Total cost to ship: $$parcel_cost </h2>
+                      ";
+            }
         ?>
     </ul>
   </body>
